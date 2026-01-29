b98a2e9f4d5001348846702fb91f6ca8bf14ed9b
Fakhri Rizha Ananda <mr.fakhririzhaa@gmail.com>
2026-01-29 11:26:09 +0700

    Refactor authentication redirection in Home component to streamline logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,5 @@ import { redirect } from "next/navigation";
 export default async function Home() {
   const session = await auth();
 
-  if (!session) {
-    redirect("/login");
-  }
-
-  redirect("/dashboard");
+  redirect(session ? "/dashboard" : "/login");
 }
